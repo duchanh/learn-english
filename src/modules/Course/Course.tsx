b36d8fcb6f560345967748dd75aa76add2661f48
Duc Hanh <hanhtd@meeyland.com>
2023-05-17 21:38:33 +0700

    Update course read and listen

--- src/modules/Course/Course.tsx
+++ src/modules/Course/Course.tsx
@@ -1,12 +1,13 @@
-import { Layout, Space, Collapse } from 'antd';
+import { Collapse } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo'
+import clsx from 'clsx';
 
 const { Panel } = Collapse;
 interface CourseProps {
   course: any
 }
-const { Header, Footer, Sider, Content } = Layout;
 import dynamic from 'next/dynamic'
 
 const VideoPlayer = dynamic(() => import('@/components/VideoPlayer'), { ssr: false })
@@ -14,36 +15,68 @@ const VideoPlayer = dynamic(() => import('@/components/VideoPlayer'), { ssr: fal
 const Course = (props: CourseProps) => {
   const { course } = props
   const router = useRouter()
-  // const {lecture, group } = router.query
-  const group: any = router.query.group
-  const lecture: any = router.query.lecture
-  const lectureGroup = course.lecture[group]
-  const lectureItem = lectureGroup.find(item => item.index == lecture)
-  const videoUrl = lectureItem.url
+  const groupId: any = router.query.group
+  const lectureId: any = router.query.lecture
+
+  const currentGroup = course.lecture[groupId]
+  const currentLecture = currentGroup?.find(item => item.index == lectureId)
+  const videoUrl = currentLecture?.url
+
   return (
-    <div className='flex w-full h-full'>
-      <aside className='flex-[0_0_300px]'>
-        <Collapse defaultActiveKey={['1']} >
-          {course.lectureGroup.map(group => (
-            <Panel header={group.title} key={`group-${group.id}`}>
-              {course.lecture[group.id].map(lecture => (
-                <Link href={`/course?lecture=${lecture.index}&group=${group.id}`}>
-                  <div className='border-b cursor-pointer'>
-                    {lecture.title}
+    <>
+      <NextSeo
+        title="24 days"
+      />
+      <div className='flex w-full h-full'>
+        <aside className='flex-[0_0_300px]'>
+          <Collapse defaultActiveKey={[groupId]} >
+            {course.lectureGroup.map(group => (
+              <Panel header={group.title} key={group.id}>
+                {course.lecture[group.id].map(lecture => (
+                  <Link href={`/course?lecture=${lecture.index}&group=${group.id}`} className={clsx('flex border-b border-grey-100 border-solid border-[1px] border-x-0 border-t-0 last:border-0', {
+                    'bg-primary-100': lecture.index == lectureId
+                  })} >
+                    <div className='cursor-pointer px-4 py-2'>
+                      {lecture.title}
+                    </div>
+                  </Link>
+                ))}
+              </Panel>
+            ))}
+          </Collapse>
+
+        </aside>
+        <main className='flex-auto'>
+          {currentLecture.type === 'read' && (
+            <div>
+              <img src={currentLecture.img} className="w-full h-full" />
+            </div>
+          )}
+
+          {currentLecture.type === 'listen' && (
+            <>
+              {currentLecture.mp3 && (
+                <>
+                  <div className='flex justify-center'>
+                    <audio controls>
+                      <source src={currentLecture.mp3} type="audio/mpeg" />
+                    </audio>
                   </div>
-                </Link>
-              ))}
-            </Panel>
+                  <div>
+                    <img src={currentLecture.img} className="w-full" />
+                  </div>
+                </>
+              )}
+            </>
+          )}
 
-          ))}
+          {currentLecture.url && (
+            <VideoPlayer videoUrl={videoUrl} />
+          )}
 
-        </Collapse>
-
-      </aside>
-      <main className='flex-auto'>
-        <VideoPlayer videoUrl={videoUrl} />
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   )
 }
 
